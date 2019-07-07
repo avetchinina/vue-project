4f478e87a5521d0cdb4bb80f352d3e48be75c0f9
AVetchinina <avetchinina@gmail.com>
2019-07-07 21:42:40 +0300

    заготовки страниц и компонентов списка пользователей и редактирования

--- js/app.js
+++ js/app.js
@@ -1,19 +1,51 @@
-var vm = new Vue({
+window.vm = new Vue({
 	el: '#app',
+	components: {
+		'users-list': {
+			template: '#userListTpl',
+			props: {
+				users: {
+					required: true,
+					type: Array
+				}
+			},
+			data: function() {
+				return {
+					isShow: true
+				}
+			},
+			methods: {
+			  toggleUserList: function() {
+				this.isShow = !this.isShow;
+			  }
+			},
+		},
+		'user-edit': {
+			template: '#editFormTpl',
+			props: {
+				user: {
+					required: true,
+					type: Object
+				}
+			},
+		}
+	},
 	data: function() {
 	  return {
-		users: [],
 		noPic: 'https://www.esvika.ee/wp-content/uploads/anon.png',
-		isShow: true,
+		users: [{}],
 	  };
 	},
 	methods: {
-	  getFullName: function(user) {
-		return user.surname + ' ' + user.name + ' ' + user.midName;
-	  },
-	  toggleUserList: function() {
-		this.isShow = !this.isShow;
-	  }
+		loadData: function() {
+			var self = this;
+			
+			axios.get('http://jsonplaceholder.typicode.com/users')
+				.then(function(response) {
+					console.log('Данные загружены!');
+					self.users = response.data;
+				});
+		}
 	},
 	computed: {
 	  countUsers: function() {
@@ -30,13 +62,6 @@ var vm = new Vue({
 	  }
 	},
 	created: function() {
-	  this.users = [
-		  {id: 1, surname: 'Иванов', name: 'Иван', midName: 'Иванович', pic: 'http://s1.iconbird.com/ico/2013/12/505/w450h4001385925286User.png'},
-		  {id: 2, surname: 'Юрьев', name: 'Алексей', midName: 'Александрович', pic: ''},
-		  {id: 3, surname: 'Петров', name: 'Федор', midName: 'Юрьевич', pic: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXblHsRy_JIWpIpJ0gB_B-ov43YtiTIbGnK4xlgTF1sXtTRTCL'},
-		  {id: 4, surname: 'Виноградова', name: 'Валентина', midName: 'Михайловна', pic: 'https://icons8.com/iconizer/files/Free_pretty_office_Icons_part_4/orig/female-user-help.png'},
-		  {id: 5, surname: 'Иванова', name: 'Людмила', midName: 'Дмитриевна', pic: 'https://pandia.ru/user/content/user/12400/photo/1449311674qoh8k.png'},
-		  {id: 6, surname: 'Курочкин', name: 'Павел', midName: 'Петрович'}
-		];
+		this.loadData();
 	}
   });
