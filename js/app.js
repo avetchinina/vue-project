46be8d2248f1e00746df906ad8131bdc66c93389
Alexandra <avetchinina@gmail.com>
2019-07-08 13:53:49 +0300

    добавлен спиннер для загрузки, отображение предупреждения, если пользователь не найден

--- js/app.js
+++ js/app.js
@@ -1,39 +1,57 @@
+var usersList = {
+	template: '#userListTpl',
+	props: {
+		users: {
+			required: true,
+			type: Array
+		},
+	},
+	data: function() {
+		return {
+			isShow: true
+		}
+	},
+	computed: {
+	  haveUsers: function() {
+		return this.users.length > 0;
+	  }
+	},
+	methods: {
+	  toggleUserList: function() {
+		this.isShow = !this.isShow;
+	  }
+	},
+};
+var usersEdit = {
+	template: '#editFormTpl',
+	props: {
+		user: {
+			required: true,
+			type: Object,
+			default: function() {
+				return {};
+			}
+		},
+		load: Boolean
+	},
+	computed: {
+	  ifUser: function() {
+		return Object.keys(this.user).length > 0;
+	  }
+	},
+};
+
 window.vm = new Vue({
 	el: '#app',
 	components: {
-		'users-list': {
-			template: '#userListTpl',
-			props: {
-				users: {
-					required: true,
-					type: Array
-				}
-			},
-			data: function() {
-				return {
-					isShow: true
-				}
-			},
-			methods: {
-			  toggleUserList: function() {
-				this.isShow = !this.isShow;
-			  }
-			},
-		},
-		'user-edit': {
-			template: '#editFormTpl',
-			props: {
-				user: {
-					required: true,
-					type: Object
-				}
-			},
-		}
+		'users-list': usersList,
+		'user-edit': usersEdit
 	},
 	data: function() {
 	  return {
 		noPic: 'https://www.esvika.ee/wp-content/uploads/anon.png',
-		users: [{}],
+		users: [],
+		wasLoaded: false
 	  };
 	},
 	methods: {
@@ -43,6 +61,7 @@ window.vm = new Vue({
 			axios.get('https://jsonplaceholder.typicode.com/users')
 				.then(function(response) {
 					console.log('Данные загружены!');
+					self.wasLoaded = true;
 					self.users = response.data;
 				});
 		}
